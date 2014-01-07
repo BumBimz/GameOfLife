9264271f2f73ffb96d43e6164a1c29c811e5c5d6
BumBimz <l3uml3imz@gmail.com>
2014-01-07 15:30:28 +0700

    Refactor Test

--- test/RuleTest.php
+++ test/RuleTest.php
@@ -1,16 +1,17 @@
 <?php
-  class RuleTest extends PHPUnit_Framework_TestCase{
+class RuleTest extends PHPUnit_Framework_TestCase{
+    function setUp(){
+      $this->rule = new Rule();
+    }
     function testRuleOneGiveAliveCellAndNeighborLessThanTwoWhenCallNextGenerationThenReturnDeadCell(){
       $expected = FALSE;
-      $rule = new Rule();
-      $actual = $rule->nextGeneration(TRUE,1);
+      $actual = $this->rule->nextGeneration(TRUE,1);
       $this->assertEquals($expected,$actual);
     }
 
     function testRuleTwoGiveAliveCellAndTwoNeighborWhenCallNextGenerationThenReturnAliveCell(){
       $expected = TRUE;
-      $rule = new Rule();
-      $actual = $rule->nextGeneration(TRUE,2);
+      $actual = $this->rule->nextGeneration(TRUE,2);
       $this->assertEquals($expected,$actual);
     }
   }
