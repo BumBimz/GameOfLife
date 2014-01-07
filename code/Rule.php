84ea1039362d460bb8af5555decb569309821a00
BumBimz <l3uml3imz@gmail.com>
2014-01-07 16:00:27 +0700

    Add Code

--- code/Rule.php
+++ code/Rule.php
@@ -1,7 +1,9 @@
 <?php
   class Rule{
     function nextGeneration($statusOfCell,$numberOfNeighbor){
-      if( $numberOfNeighbor == 2 || $numberOfNeighbor == 3 )
+      if( $numberOfNeighbor == 2 && $statusOfCell == TRUE )
+        return TRUE;
+      if( $numberOfNeighbor == 3 )
         return TRUE;
     }
   }
