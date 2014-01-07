b8efa1df794561fab847fe0813c9250dbb71b591
BumBimz <l3uml3imz@gmail.com>
2014-01-07 14:59:46 +0700

    Add Test Rule Two When Two Neighbor

--- test/RuleTest.php
+++ test/RuleTest.php
@@ -6,5 +6,12 @@
       $actual = $rule->nextGeneration(TRUE,1);
       $this->assertEquals($expected,$actual);
     }
+
+    function testRuleTwoGiveAliveCellAndTwoNeighborWhenCallNextGenerationThenReturnAliveCell(){
+      $expected = TRUE;
+      $rule = new Rule();
+      $actual = $rule->nextGeneration(TRUE,2);
+      $this->assertEquals($expected,$actual);
+    }
   }
 ?>
