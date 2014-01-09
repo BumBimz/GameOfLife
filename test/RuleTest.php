d2c2c41383ce2ac2d2560bfe7482fc93fbce4db1
BumBimz <l3uml3imz@gmail.com>
2014-01-09 12:39:56 +0700

    Refactor

--- test/RuleTest.php
+++ test/RuleTest.php
@@ -3,37 +3,37 @@ class RuleTest extends PHPUnit_Framework_TestCase{
     function setUp(){
       $this->rule = new Rule();
     }
-    function testRuleOneGiveAliveCellAndNeighborLessThanTwoWhenCallNextGenerationThenReturnDeadCell(){
+    function testRuleOneWhenCallAliveCellHaveNeighborLessTwoThenCellDie(){
       $expected = FALSE;
       $actual = $this->rule->ruleOfGame(TRUE,1);
       $this->assertEquals($expected,$actual);
     }
 
-    function testRuleTwoGiveAliveCellAndTwoNeighborWhenCallNextGenerationThenReturnAliveCell(){
+    function testRuleTwoWhenAliveCellHaveTwoNeighborThenCellAlive(){
       $expected = TRUE;
       $actual = $this->rule->ruleOfGame(TRUE,2);
       $this->assertEquals($expected,$actual);
     }
 
-    function testRuleTwoGiveAliveCellAndThreeNeighborWhenCallNextGenerationThenReturnAliveCell(){
+    function testRuleTwoWhenAliveCellHaveThreeNeighborThenCellAlive(){
       $expected = TRUE;
       $actual = $this->rule->ruleOfGame(TRUE,3);
       $this->assertEquals($expected,$actual);
     }
 
-    function testRuleThreeGiveAliveCellAndNeighborMoreThreeWhenCallNextGenerationThenReturnDeadCell(){
+    function testRuleThreeWhenAliveCellHaveNeighborMoreThreeThenCellDie(){
       $expected = FALSE;
       $actual = $this->rule->ruleOfGame(TRUE,4);
       $this->assertEquals($expected,$actual);
     }
     
-    function testRuleFourGiveDeadCellAndThreeNeighborWhenCallNextGenerationThenReturnAliveCell(){
+    function testRuleFourWhenDeadCellHaveThreeNeighborThenCellReborn(){
       $expected = TRUE;
       $actual = $this->rule->ruleOfGame(FALSE,3);
       $this->assertEquals($expected,$actual);
     }
 
-    function testRuleFourGiveDeadCellAndTwoNeighborWhenCallNextGenerationThenReturnDeadCell(){
+    function testRuleFourWhenDeadCellHaveTwoNeighborThenCellDie(){
       $expected = FALSE;
       $actual = $this->rule->ruleOfGame(FALSE,2);
       $this->assertEquals($expected,$actual);
