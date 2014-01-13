d8203c00c5cb1ea1a2a92c8822cd7e46f3762cb1
BumBimz <l3uml3imz@gmail.com>
2014-01-13 23:35:08 +0700

    refactor

--- code/Cell.php
+++ code/Cell.php
@@ -9,10 +9,12 @@
     }
 
     function sight($x,$y){
-      $neighbor =0 ;
-      for($i=$x-1;$i<=$x+1;$i++){
-        for($j=$y-1;$j<=$y+1;$j++){
-          if(($i==$x&&$j==$y) == False)
+      $neighbor = 0;
+      $sightX = [$x-1,$x+1];
+      $sightY = [$y-1,$y+1];
+      for($i = $sightX[0];$i <= $sightX[1];$i++){
+        for($j = $sightY[0];$j <= $sightY[1];$j++){
+          if($i!=$x||$j!=$y)
             $neighbor+= $this->checkStatusCell($this->univers[$i][$j]);
         }
       }
--- test/CellTest.php
+++ test/CellTest.php
@@ -1,54 +1,30 @@
 <?php
-  class CellTest extends PHPUnit_Framework_TestCase{
-    function testCheckStatusCellWhenPositionAliveCellThenReturnAliveCell(){
-      $aroundCell=[[TRUE,FALSE],
-                  [FALSE,TRUE]];
-      $expected = TRUE;
-      $cell = new Cell();
-      $actual = $cell->checkStatusCell($aroundCell[0][0]);
-      $this->assertEquals($expected,$actual);
-    }
-
-    function testSetUniverWhenGetUniverThenReturnUnivers(){
+class CellTest extends PHPUnit_Framework_TestCase{
+    function setUp(){
+      $this->cell = new Cell();
       $univers=[[TRUE,TRUE,TRUE,TRUE,TRUE],
                   [TRUE,TRUE,TRUE,TRUE,TRUE],
                   [TRUE,TRUE,TRUE,FALSE,TRUE],
                   [TRUE,FALSE,TRUE,TRUE,FALSE],
                   [TRUE,TRUE,TRUE,TRUE,TRUE]];
-      
-      $expected=[[TRUE,TRUE,TRUE,TRUE,TRUE],
-                  [TRUE,TRUE,TRUE,TRUE,TRUE],
-                  [TRUE,TRUE,TRUE,FALSE,TRUE],
-                  [TRUE,FALSE,TRUE,TRUE,FALSE],
-                  [TRUE,TRUE,TRUE,TRUE,TRUE]];
-      $cell = new Cell();
-      $cell->setUnivers($univers);
-      $actual = $cell->univers;
+      $this->cell->setUnivers($univers);
+    }
+
+    function testCheckStatusCellWhenPositionAliveCellThenReturnAliveCell(){
+      $expected = TRUE;
+      $actual = $this->cell->checkStatusCell($this->cell->univers[0][0]);
       $this->assertEquals($expected,$actual);
     }
+
     function testSightCellWhenMiddleBoardThenReturnEight(){
-      $boardCell=[[TRUE,TRUE,TRUE,TRUE,TRUE],
-                  [TRUE,TRUE,TRUE,TRUE,TRUE],
-                  [TRUE,TRUE,TRUE,FALSE,TRUE],
-                  [TRUE,FALSE,TRUE,TRUE,FALSE],
-                  [TRUE,TRUE,TRUE,TRUE,TRUE]];
       $expected = 8;
-      $cell = new Cell();
-      $cell->setUnivers($boardCell);
-      $actual = $cell->sight(1,1);
+      $actual = $this->cell->sight(1,1);
       $this->assertEquals($expected,$actual);
     }
     
     function testSightCellWhenMiddleBoardThenReturnSeven(){
-      $boardCell=[[TRUE,TRUE,TRUE,TRUE,TRUE],
-                  [TRUE,TRUE,TRUE,TRUE,TRUE],
-                  [TRUE,TRUE,TRUE,FALSE,TRUE],
-                  [TRUE,FALSE,TRUE,TRUE,FALSE],
-                  [TRUE,TRUE,TRUE,TRUE,TRUE]];
       $expected = 7;
-      $cell = new Cell();
-      $cell->setUnivers($boardCell);
-      $actual = $cell->sight(1,2);
+      $actual = $this->cell->sight(1,2);
       $this->assertEquals($expected,$actual);
     }
   }
