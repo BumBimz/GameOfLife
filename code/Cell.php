64e40c3fff792dce6249f185ddedf78bcf49bca3
BumBimz <l3uml3imz@gmail.com>
2014-01-13 21:47:45 +0700

    Add new test

--- code/Cell.php
+++ code/Cell.php
@@ -3,5 +3,9 @@
     function checkStatusCell($neighbor){
       return $neighbor;
     }
+
+    function sight($x,$y){
+      return 8;
+    }
   }
 ?>
--- test/CellTest.php
+++ test/CellTest.php
@@ -5,7 +5,19 @@
                   [FALSE,TRUE]];
       $expected = TRUE;
       $cell = new Cell();
-      $actual= $cell->checkStatusCell($aroundCell[0][0]);
+      $actual = $cell->checkStatusCell($aroundCell[0][0]);
+      $this->assertEquals($expected,$actual);
+    }
+
+    function testSightCellWhenMiddleBoardThenReturnEight(){
+      $boardCell=[[TRUE,TRUE,TRUE,TRUE,TRUE],
+                  [TRUE,TRUE,TRUE,TRUE,TRUE],
+                  [TRUE,TRUE,TRUE,FALSE,TRUE],
+                  [TRUE,FALSE,TRUE,TRUE,FALSE],
+                  [TRUE,TRUE,TRUE,TRUE,TRUE]];
+      $expected = 8;
+      $cell = new Cell();
+      $actual = $cell->sight(1,1);
       $this->assertEquals($expected,$actual);
     }
   }
