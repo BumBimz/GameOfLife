0c50f041594e79160d5fcf99d43c756f696efe8f
BumBimz <l3uml3imz@gmail.com>
2014-01-16 23:36:54 +0700

    Add Create Universe

--- code/Universe.php
+++ code/Universe.php
@@ -3,7 +3,22 @@
     function setRandomer($randomer){
       $this->randomer = $randomer;
     } 
+    
+    function setHeightUniverse($heightUniverse){
+      $this->heightUniverse =$heightUniverse;
+    }
+    
+    function setWidthUniverse($widthUniverse){
+      $this->widthUniverse =$widthUniverse;
+    }
 
+    function createUniverse(){
+      for($i=0;$i<$this->heightUniverse;$i++){
+        for($j=0;$j<$this->widthUniverse;$j++){
+          $this->universe[$i][$j] = $this->getCell();
+        }
+      }
+    }
     function getCell(){
       return $this->randomer->random() == 1;
     }
--- test/UniverseTest.php
+++ test/UniverseTest.php
@@ -10,5 +10,22 @@
       $actual = $universe->getCell();
       $this->assertTrue($actual);
     }
+
+    function testCreateUniverseWhenUniverseIsFiveXFourThenReturnTwentyCell(){
+      $excepted = [[TRUE,TRUE,TRUE,TRUE,TRUE],
+       [TRUE,TRUE,TRUE,TRUE,TRUE],
+       [TRUE,TRUE,TRUE,TRUE,TRUE],
+       [TRUE,TRUE,TRUE,TRUE,TRUE]];
+      $randomer = $this->getMock('Randomer',array('random'));
+      $randomer->expects($this->exactly(20))
+        ->method('random')
+        ->will($this->returnValue(1));
+      $universe = new Universe();
+      $universe->setHeightUniverse(4);
+      $universe->setWidthUniverse(5);
+      $universe->setRandomer($randomer);
+      $universe->createUniverse();
+      $this->assertEquals($excepted,$universe->universe);
+    }
   }
 ?>
